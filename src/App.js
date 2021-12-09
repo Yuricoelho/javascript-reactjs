5a886e2d8163df09f4312f781c8be915361a34a8
Yuri <yuricoelho33@gmail.com>
2021-12-09 11:32:16 -0300

    Inserindo arquivos css para estilizar a aplicação.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import ListaDeNotas from "./components/ListaDeNotas";
-import FormularioCadastro from "./components/FormularioCadastro";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 import { Component } from "react";
+import "./assets/App.css";
+import './assets/index.css';
 
 class App extends Component{
 
   render(){
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro/>
         <ListaDeNotas/>
       </section>
